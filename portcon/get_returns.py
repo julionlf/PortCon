f27232fc51461b6dbe1050cb48217c0c777182e5
julionlf <jlebron.feliciano@gmail.com>
2021-06-05 01:31:35 -0400

    Added lines to read CSV file of stock prices

--- portcon/get_returns.py
+++ portcon/get_returns.py
@@ -1,1 +1,26 @@
-print("Hello world!")
+# Import libraries
+import numpy as np
+import pandas as pd
+import datetime
+
+# User Inputs
+path2data = 'C:\\Users\\Julio\\Downloads\\'
+dataFiles = ["BTC-USD","ETH-USD"]
+startYear = 2020
+startMonth = 4
+startDay = 5
+endYear = 2021
+endMonth = 4
+endDay = 4
+
+# Process data
+startDate = datetime.date(startYear,startMonth,startDay)
+endDate = datetime.date(endYear,endMonth,endDay)
+fullPath = path2data+dataFiles[0]+".csv"
+assetData = pd.read_csv(fullPath,header = 0, index_col="Date", parse_dates=True)
+returns = assetData["Adj Close"].pct_change()
+returns = returns.dropna()
+
+for i in range(1,13,1):
+    currDate = (startDate + pd.offsets.DateOffset(months=i)).date()
+    print(returns[currDate])
