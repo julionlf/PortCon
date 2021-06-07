fa35c5fea2a0276076e753e85d73a99971b89f1a
julionlf <jlebron.feliciano@gmail.com>
2021-06-07 13:41:55 -0400

    Created main.py and created modelling class with covariance materix function.

---
+++ portcon/modeling.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+class Modelling:
+
+    def __init__(self):
+        print("Hello from Modelling class.")
+
+    def covariance(returns):
+        print("Hello from covariance class.")
