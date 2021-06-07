0df6bcb762d45adbb58544998365d5db5974dc0f
julionlf <jlebron.feliciano@gmail.com>
2021-06-07 16:58:40 -0400

    Wrote a few lines to test implementation of typical modeling workflow.

---
+++ portcon/main.py
@@ -0,0 +1,18 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+from modeling import Modeling as mdl
+
+# User Inputs
+path2data = 'C:\\Users\\Julio\\Downloads\\'
+dataFiles = ["Crypto_Returns"]
+
+# Import Data
+fullPath = path2data+dataFiles[0]+".csv"
+returns = pd.read_csv(fullPath,header = 0, index_col="Date", parse_dates=True)
+
+# Create model object
+model = mdl(returns)
+
+# Print Covariance Matrix
+print(model.covariance())
