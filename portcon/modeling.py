086f09ab79df29471c357738d705e9a1b7aa160e
julionlf <jlebron.feliciano@gmail.com>
2021-06-07 16:56:55 -0400

    Wrote function to calculate stationary risk.

--- portcon/modeling.py
+++ portcon/modeling.py
@@ -1,10 +1,28 @@
+# Modelling Class:
+#
+# DESCRIPTION:
+#   Contains various routines to model risk and expected returns
+#   of a collection of assets.
+
 import numpy as np
 import pandas as pd
 
-class Modelling:
+class Modeling:
 
-    def __init__(self):
-        print("Hello from Modelling class.")
+    def __init__(self, returns):
+        self.returns = returns
 
-    def covariance(returns):
-        print("Hello from covariance class.")
+    def covariance(self):
+    # DESCRIPTION:
+    #   Computes the stationary variance-covariance matrix
+    #   of a collection of assets.
+    #
+    # INPUTS:
+    #   returns - pandas dataframe of returns. Index are dates,
+    #             rows are returns at any given date, columns
+    #             are the returns for any given asset.
+    #             
+    # OUTPUTS
+    #   sigma - NxN numpy array of doubles.
+    #
+        return np.cov(self.returns.values)
