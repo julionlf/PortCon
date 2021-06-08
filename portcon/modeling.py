754efca86d001dae541e93d76a9f3a6a479b4758
julionlf <jlebron.feliciano@gmail.com>
2021-06-07 21:54:03 -0400

    Wrote portfolio risk method.

--- portcon/modeling.py
+++ portcon/modeling.py
@@ -13,7 +13,12 @@ import pandas as pd
 
 class Modeling:
 
-    def __init__(self, returns=None, weights=None, asset_returns=None):
+    def __init__(
+    self, returns=None, 
+    weights=None, 
+    asset_returns=None, 
+    asset_risks=None):
+
         if returns is None:
             self.returns = []
         else:
@@ -28,6 +33,11 @@ class Modeling:
             self.asset_returns = []
         else:
             self.asset_returns = asset_returns
+
+        if asset_risks is None:
+            self.asset_risks = []
+        else:
+            self.asset_risks = asset_risks
 
     def covariance(self,returns=None):  
     # DESCRIPTION:
@@ -85,3 +95,24 @@ class Modeling:
             weights = self.weights
             
         return weights.T @ asset_returns
+
+    def portfolio_risk(self, asset_risks=None, weights=None):
+    # DESCRIPTION:
+    #   Computes the risk of a portfolio of assets.
+    #
+    # INPUTS:
+    #   weights - 1D numpy array of doubles between 0 and 1, that 
+    #             add up to 1. These are the weights allocated to
+    #             each asset in the portfolio.
+    #   asset_risks - 2D array od doubles. Variance-covariance matrix
+    #                 of the portfolio assets.
+    #             
+    # OUTPUTS
+    #   sigmap - scalar double. Risk of the portfolio
+    
+        if asset_risks is None:
+            asset_risks = self.asset_risks
+        if weights is None:
+            weights = self.weights
+            
+        return weights.T @ asset_risks @ weights        
