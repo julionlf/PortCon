0ac37e5c829f8af5e7cf1791b39d6494ea1c512b
julionlf <jlebron.feliciano@gmail.com>
2021-06-07 21:23:56 -0400

    Updated default values in methods' arguments.

--- portcon/main.py
+++ portcon/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from modeling import Modeling as mdl
 
 # User Inputs
+weights = np.repeat(1/4,6)
 path2data = 'C:\\Users\\Julio\\Downloads\\'
 dataFiles = ["Crypto_Returns"]
 
@@ -12,10 +13,12 @@ fullPath = path2data+dataFiles[0]+".csv"
 returns = pd.read_csv(fullPath,header = 0, index_col="Date", parse_dates=True)
 
 # Create model object
-model = mdl(returns)
+model = mdl()
+print(model.portfolio_return(model.exp_returns(returns),weights))
+
 
 # Print Stationary Covariance Matrix
-print(model.covariance())
+#print(model.covariance())
 
 # Print Stationary Exp. Returns Vector
-print(model.exp_returns())
+#print(model.exp_returns())
--- portcon/modeling.py
+++ portcon/modeling.py
@@ -1,18 +1,35 @@
-# Modelling Class:
-#
-# DESCRIPTION:
-#   Contains various routines to model risk and expected returns
-#   of a collection of assets.
+"""
+Modelling Class:
+
+DESCRIPTION:
+    Contains various methods to model risk and expected returns
+    of a collection of assets. It also contains methods to compute
+    portfolio risk and return.
+
+"""
 
 import numpy as np
 import pandas as pd
 
 class Modeling:
 
-    def __init__(self, returns):
-        self.returns = returns
+    def __init__(self, returns=None, weights=None, asset_returns=None):
+        if returns is None:
+            self.returns = []
+        else:
+            self.returns = returns
 
-    def covariance(self):
+        if weights is None:
+            self.weights = []
+        else:
+            self.weights = weights
+
+        if asset_returns is None:
+            self.asset_returns = []
+        else:
+            self.asset_returns = asset_returns
+
+    def covariance(self,returns=None):  
     # DESCRIPTION:
     #   Computes the stationary variance-covariance matrix
     #   of a collection of assets.
@@ -24,10 +41,14 @@ class Modeling:
     #             
     # OUTPUTS
     #   sigma - NxN numpy array of doubles.
-    
-        return np.cov(self.returns.values)
 
-    def exp_returns(self):
+            if returns is None:
+                return np.cov(self.returns.values)
+            else:
+                return np.cov(returns.values)
+            
+
+    def exp_returns(self, returns=None):
     # DESCRIPTION:
     #   Computes the stationary expected returns vector
     #   of a collection of assets.
@@ -38,6 +59,29 @@ class Modeling:
     #             are the returns for any given asset.
     #             
     # OUTPUTS
-    #   sigma - Nx1 numpy array of doubles.
+    #   r - Nx1 numpy array of doubles.
     
-        return np.average(self.returns.values,axis=0)
+        if returns is None:
+            return np.average(self.returns.values,axis=0)
+        else:
+            return np.average(returns.values,axis=0)
+
+    def portfolio_return(self, asset_returns=None, weights=None):
+    # DESCRIPTION:
+    #   Computes the expected return of a portfolio of assets.
+    #
+    # INPUTS:
+    #   weights - 1D numpy array of doubles between 0 and 1, that 
+    #             add up to 1. These are the weights allocated to
+    #             each asset in the portfolio.
+    #   exp_returns - expected return of each assset in the portfolio
+    #             
+    # OUTPUTS
+    #   rp - scalar double. Expected return of the portfolio
+    
+        if asset_returns is None:
+            asset_returns = self.asset_returns
+        if weights is None:
+            weights = self.weights
+            
+        return weights.T @ asset_returns
