62f4ed4c8c6e5fe686be8767335f8fa9cd966cf3
julionlf <jlebron.feliciano@gmail.com>
2021-06-10 00:12:17 -0400

    Wrote crode to bring in sector indeces data from Coursera class.

--- portcon/main.py
+++ portcon/main.py
@@ -5,27 +5,45 @@ from modeling import Modeling as mdl
 from assetalloc import Asset_Allocation as aa
 
 # User Inputs
-weights = np.repeat(1/7,7)
-bounds = ((0,1),)*7
-path2data = 'C:\\Users\\Julio\\Downloads\\'
-dataFiles = ["Crypto_Returns"]
+weights = np.repeat(1/2,2)
+bounds = ((0,1),)*2
+path2data = 'C:\\Users\\Julio\\Projects\\Finance_Data\\data\\'
+dataFiles = ['ind30_m_vw_rets']
 
 # Import Data
 fullPath = path2data+dataFiles[0]+".csv"
-returns = pd.read_csv(fullPath,header = 0, index_col="Date", parse_dates=True)
+returns = pd.read_csv(fullPath,header = 0, index_col=0, parse_dates=True)/100
+returns.index = pd.to_datetime(returns.index, format ="%Y%m").to_period("M")
+returns.columns = returns.columns.str.strip()
+returns = returns[["Games","Steel"]]
 
-# Create model object
+# User Inputs Crypto
+#weights = np.repeat(1/7,7)
+#bounds = ((0,1),)*2
+#path2data = 'C:\\Users\\Julio\\Downloads\\'
+#dataFiles = ["Crypto_Returns"]
+
+# Import Crypto Data
+#returns = pd.read_csv(fullPath,header = 0, index_col="Date", parse_dates=True)
+
+print(returns.head())
+
+# Create model objectclea
 model = mdl()
 sigma = model.covariance(returns)
 asset_returns = model.exp_returns(returns)
-print('Portfolio Expected Return:')
+print('Portfolio Risk')
 print(model.portfolio_risk(weights, sigma))
-print('Portfolio Risk:')
+print('Portfolio  Expected Return:')
 print(model.portfolio_return(weights, asset_returns))
+print("Asset Returns:")
+print(asset_returns)
 
 # Create asset allocation object
 allocation = aa()
 print("Compute return target portfolio:")
-print(allocation.minimize_vol(weights,sigma,bounds,0.3,asset_returns))
-print(allocation.msr(weights,sigma,bounds,0.03,asset_returns))
-print(allocation.gmv(weights,sigma,bounds,0.03,asset_returns))
+print(allocation.minimize_vol(weights,sigma,bounds,0.01,asset_returns))
+print("Compute MSR Portfolio:")
+print(allocation.msr(weights,sigma,bounds,0.0003,asset_returns))
+print("Compute GMV Portfolio:")
+print(allocation.gmv(weights,sigma,bounds))
