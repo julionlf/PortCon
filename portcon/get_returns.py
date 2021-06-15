c73d518ac94970cf338b8a0d819823f53c457b2f
julionlf <jlebron.feliciano@gmail.com>
2021-06-15 15:01:35 -0400

    Created get_returns class.

--- portcon/get_returns.py
+++ portcon/get_returns.py
@@ -1,36 +1,21 @@
-# Import libraries
+"""
+Get Returns Class:
+
+DESCRIPTION:
+    Contains various methods to convert returns to a specific
+    frequency period. For example: daily to monthly daily to 
+    yearly, monthly to quarterly, etc...
+
+"""
+
 import numpy as np
 import pandas as pd
-from modeling import Modeling as mdl
 
-#import datetime
+class Get_Returns:
 
-# User Inputs
-path2data = 'C:\\Users\\Julio\\Downloads\\'
-dataFiles = ["Crypto_Returns"]
-startYear = 2020
-startMonth = 4
-startDay = 5
-endYear = 2021
-endMonth = 4
-endDay = 4
-
-# Process data
-#startDate = datetime.date(startYear,startMonth,startDay)
-#endDate = datetime.date(endYear,endMonth,endDay)
-fullPath = path2data+dataFiles[0]+".csv"
-returns = pd.read_csv(fullPath,header = 0, index_col="Date", parse_dates=True)
-#assetData.fillna(0)
-#print(assetData.head())
-#returns = assetData["Adj Close"].pct_change()
-#returns = returns.dropna()
-
-#sigma = np.cov(assetData.values)
-model = mdl(returns)
-print(model.covariance())
-#sigma = mdl.Modeling.covariance(assetData.values)
-#print(sigma)
-
-#for i in range(1,13,1):
-#    currDate = (startDate + pd.offsets.DateOffset(months=i)).date()
-#    print(returns[currDate])
+    def __init__(self,returns=None):
+        
+        if returns is None:
+            self.returns = []
+        else:
+            self.returns = returns
